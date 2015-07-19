b702c1e9f645362a60cec279c7c386fa6950300a
BirdHK <huangkunniao@163.com>
2015-07-19 17:07:55 +0800

    更新快排的错误，代码更简单

--- src/com/hk/QuickSork.java
+++ src/com/hk/QuickSork.java
@@ -6,32 +6,35 @@ public class QuickSork {
 	//但若初始序列按关键码有序或基本有序时，快排序反而蜕化为冒泡排序
 	public static void main(String[] args) 
 	{
-		int a[] = {12,32,10,76,20,2,19,213,345,56,23};
-		myQuickSort(a,0,10);
+		int a[] = {12,32,10,76,20,2,19,213,345,56,23,567,456,89};
+		myQuickSort(a,0,13);
+		
 		for(int i : a)
 			System.out.print(i+" ");
 	}
 
-	private static void myQuickSort(int a[],int low, int high)
+	//快速排序
+	private static void myQuickSort(int s[], int l, int r)
 	{
-		if(low < high){  
-	        int privotLoc = partition(a,  low,  high);  //将表一分为二  
-	        myQuickSort(a,  low,  privotLoc -1);          //递归对低子表递归排序  
-	        myQuickSort(a,   privotLoc + 1, high);        //递归对高子表递归排序  
-	    } 
-	}
-	
-	private static int partition(int a[],int low,int high)
-	{
-			int privotKey = a[low];                            	 //基准元素  
-		    while(low < high)										 //从表的两端交替地向中间扫描 
-		    {          
-		    	//从high 所指位置向前搜索，至多到low+1 位置。将比基准元素小的交换到低端  
-		        while(low < high  && a[high] >= privotKey) --high;  
-		        a[low]=a[high];
-		        while(low < high  && a[low] <= privotKey ) ++low;  
-		       a[high] =a[low];
-		    }  
-		    return low; //返回标记元素角标
+	    if (l < r)
+	    {
+			//Swap(s[l], s[(l + r) / 2]); //将中间的这个数和第一个数交换 参见注1
+	        int i = l, j = r, x = s[l];
+	        while (i < j)
+	        {
+	            while(i < j && s[j] >= x) // 从右向左找第一个小于x的数
+					j--;  
+	            if(i < j) 
+					s[i++] = s[j];
+				
+	            while(i < j && s[i] < x) // 从左向右找第一个大于等于x的数
+					i++;  
+	            if(i < j) 
+					s[j--] = s[i];
+	        }
+	        s[i] = x;
+	        myQuickSort(s, l, i - 1); // 递归调用 
+	        myQuickSort(s, i + 1, r);
+	    }
 	}
 }
